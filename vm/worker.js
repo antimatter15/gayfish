4cd4cb549b0a250476759c8146705dab9a03b267
Kevin Kwok <antimatter15@gmail.com>
2015-06-14 21:46:26 -0400

    it now runs less

--- vm/worker.js
+++ vm/worker.js
@@ -2,6 +2,7 @@
 importScripts("tarball/dist/tarball.js")
 importScripts("utf8.js")
 importScripts("path.js")
+importScripts("process.js")
 
 var __latestCellID;
 function __track$loop__(i, total){
@@ -11,13 +12,59 @@ function __track$loop__(i, total){
 var npm_package_cache = {};
 var npm_version_cache = {}
 var npm_require_cache = {}
-
+var browserifyBuiltins = {
+	assert: 'assert',
+	buffer: 'buffer',
+	child_process: false,
+	cluster: false,
+	console: 'console-browserify',
+	constants: 'constants-browserify',
+	crypto: 'crypto-browserify',
+	dgram: false,
+	dns: false,
+	domain: 'domain-browser',
+	events: 'events',
+	fs: false,
+	http: 'http-browserify',
+	https: 'https-browserify',
+	module: false,
+	net: false,
+	os: 'os-browserify/browser.js',
+	path: 'path-browserify',
+	punycode: 'punycode',
+	querystring: 'querystring-es3',
+	readline: false,
+	repl: false,
+	stream: 'stream-browserify',
+	_stream_duplex: 'readable-stream/duplex.js',
+	_stream_passthrough: 'readable-stream/passthrough.js',
+	_stream_readable: 'readable-stream/readable.js',
+	_stream_transform: 'readable-stream/transform.js',
+	_stream_writable: 'readable-stream/writable.js',
+	string_decoder: 'string_decoder',
+	sys: 'util/util.js',
+	timers: 'timers-browserify',
+	tls: false,
+	tty: 'tty-browserify',
+	url: 'url',
+	util: 'util/util.js',
+	vm: 'vm-browserify',
+	zlib: 'browserify-zlib',
+	_process: 'process/browser'
+};
 
 function require(path){
+	console.log('req', path)
+	if(path in browserifyBuiltins && browserifyBuiltins[path] != path){
+		path = browserifyBuiltins[path]
+		if(path === false) return {};
+	}
+
 	var id = path.split("/")[0],
 		path = path.split("/").slice(1).join('/');
 	var version = id.split('@')[1] || 'latest',
 		package = id.split('@')[0];
+
 
 	// console.log('require', path, package, version)
 
@@ -53,39 +100,92 @@ function require(path){
 		console.log('downloaded', pkg._id, "(" +npm_version_cache[pkg._id].length+" files)");
 	}
 	
-	if(!path) path = pkg.main;
+	if(!path){
+		if(typeof pkg.browser == 'string'){
+			path = pkg.browser
+		}else if(pkg.main){
+			path = pkg.main
+		}else{
+			path = 'index.js'
+		}
+	}
+	
+	// console.log(pkg._id, path)
 	return require_relative(pkg, Path.relative('', path));
 }
 
 
 function require_relative(pkg, path){
 	var files = npm_version_cache[pkg._id];
+	var include_prefix = ['', '.js', '.json'];
 
+	// TODO: make this algorithm make more sense 
+	if(typeof pkg.browser == 'object'){
+		var canonical_mapping = {}
+		Object.keys(pkg.browser).forEach(function(e){ 
+			canonical_mapping[Path.relative('', e)] = pkg.browser[e] && Path.relative('', pkg.browser[e])
+		});
+		var match = Object.keys(canonical_mapping).filter(function(n){
+			return include_prefix.some(function(prefix){
+				return (n == path + prefix || n == path + '/index' + prefix)	
+			})
+		})[0];
+		if(match){
+			path = canonical_mapping[match];
+			if(path === false) return {};
+		}
+	}
+		
+		
 	function locate(name){
-		// name = name.replace(/)
 		return files.filter(function(n){
-			return ['', '.js'].some(function(prefix){
+			return include_prefix.some(function(prefix){
 				return (n.filename == name + prefix || n.filename == name + '/index' + prefix)	
 			})
 		})[0]
 	}
 	var file = locate(path);
+
+	if(!file){
+		throw new Error("File "+path+" not found in package " + pkg._id)
+	}
 	// 
 	// console.log(pkg, path, file)
 
 	var modules = npm_require_cache[pkg._id];
+	if(file.filename in modules) return modules[file.filename].exports;
 
-	if(!(file.filename in modules)){
+	
+	if(/\.json$/.test(file.filename)){
+		// load JSON
+		var module = {
+			exports: JSON.parse(file.data)
+		}
+	}else{
+		// load javascript
+		var extras = "";
+		if(pkg.name == 'http-browserify' || pkg.name == 'https-browserify'){
+			extras += "var window = self;"; // get it to work in a webworker
+		}
 		var code = "(function(module){\
-			var exports = module.exports, require = module.require;\
+			var global = self, exports = module.exports, require = module.require, process = module.process; " + extras + " \n\
 			" + file.data + "\
-			;return module})(prepare_module.apply(null, " + 
+			\n\n;return module})(prepare_module.apply(null, " + 
 				JSON.stringify([pkg._id, file.filename]) + "))";
-		// console.log(code)
-		modules[file.filename] = eval.call(null, code);	
-	}
+		try {
+			var module = eval.call(null, code);
+		} catch (err) {
+			console.groupCollapsed('code')
+			console.info(file.filename, pkg._id)
+			console.log(code)
+			console.groupEnd('code')
+			console.error(err)
+			// throw err;
 
-	return modules[file.filename].exports;
+			var module = { exports: {} }
+		}
+	}
+	return modules[file.filename] = module;
 }
 
 
@@ -95,15 +195,16 @@ function prepare_module(id, filename){
 	return {
 		exports: {},
 		require: function(path){
+			// console.log('require', path, 'from', id, filename)
 			if(path.startsWith('.')){
 				// console.log('TODO: relative requires', path, filename)
 				return require_relative(pkg, Path.join(Path.dirname(filename), path))
 			}else{
 				// console.log('submodule require', path)
-				return require(path)	
+				return require(path)
 			}
-			
-		}
+		},
+		process: process
 	}
 }
 
