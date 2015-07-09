1dc766c66d76509cc225f24b284835aeec9ede33
Kevin Kwok <antimatter15@gmail.com>
2015-07-09 15:39:06 -0700

    rewrite of object tree

--- src/result/objtree.jsx
+++ src/result/objtree.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react/addons';
 import classNames from 'classnames'
 import * as _ from 'lodash'
-import {array_join} from '../util.js'
+import {array_join} from '../util'
 
 require('./blink/consoleView.css')
 require('./blink/objectValue.css')
@@ -12,6 +12,8 @@ require('./blink/inspectorSyntaxHighlight.css')
 
 // TODO: rewrite this so it's not a gnarly mess
 
+
+// this shows shallow object descriptions
 class ObjectPreview extends Component {
     render(){
         var {node} = this.props;
@@ -29,181 +31,119 @@ class ObjectPreview extends Component {
             }else if(node.type == 'string'){
                 return <span className="cm-js-string">{'"' + node.value + '"'}</span>
             }else if(node.type == 'boolean'){
-                return <span className="object-value-boolean source-code">{node.value ? 'true' : 'false'}</span>
+                return <span className="object-value-boolean">{node.value ? 'true' : 'false'}</span>
             }else{
                 return <span>Unhandled Type: {node.type}</span>
             }
         }else{
-            return <span>Warning: Unwrapped Object</span>
+            return <span>Unwrapped {JSON.stringify(node)}</span>
         }
-
-
-
-
-        // if(typeof node == "undefined" || node === null){
-        //     return <span className="object-value-null">{node + ''}</span>
-        // }else if(typeof node == "string"){
-            
-        // }else if(typeof node == "number"){
-            
-        // }else if(typeof node == "object"){
-        //     if(Array.isArray(node) || node.type == 'array'){
-        //         return <span className="value object-value-array">Array[{node.length}]</span>;
-        //     }else{
-        //         return <span className="object-value-object">Object</span>;
-        //     }
-        // }else if(typeof node == "boolean"){
-            
-        // }else{
-        //     return <span>wat? {typeof node}</span>
-        // }
     }
 }
 
-export default class ObjectTree extends Component {
-    constructor() {
-        super()
-        this.state = {
-            expanded: false
-        }
-    }
-    toggleExpand = () => {
-        this.setState({ expanded: !this.state.expanded })
-    }
-    render() {
+// this shows a deeper description of some expandable objects (arrays, objects)
+class ObjectFatPreview extends Component {
+    render(){
         var {node} = this.props;
-        var {expanded} = this.state;
-
-        if(typeof node != "object" || node === null){
-            if(typeof node == "string" && node.length > 50){
-                return <ol className="tree-outline component-root platform-mac source-code object-properties-section">
-                    <li className={classNames({"parent": 1, expanded})} onClick={this.toggleExpand}>
-                        <div className="selection"></div>
-                        <content>
-                            <ObjectPreview node={JSON.stringify(node).slice(1, 150) + '...'} />
-                        </content>
-                    </li>
-                    {!expanded ? <ol className="children" /> : <ol className="children expanded">
-                        <span className="cm-js-string">{node}</span>
-                    </ol>}
-                </ol>
-            }else{
-                return <ObjectPreview node={node} />;   
-            }
-        }else{
-            if(node.type == "array"){
+        if(typeof node == "object"){
+            if(node.type == 'array'){
                 if(node.length < 100 && node.values){
-                    return (
-                        <span className="object-value-array">
-                            [{node.values.map((x, k) => [
-                                <ObjectTree node={x} />, 
-                                k == node.length - 1 ?  "" : ", "
-                            ])}]
-                        </span>
-                    )
-                }else{
-                    return <span onClick={this.toggleExpand}>
-                        <ObjectPreview node={node} />
-                    </span>;
+                    return <span className="object-value-array">[{
+                        array_join(node.values.map(x => <ObjectPreview node={x} />), ', ')
+                    }]</span>
                 }
-            }else if(node.type == "object"){
+            }else if(node.type == 'object' && node.pairs){
                 var keys = _.pluck(node.pairs, 0),
                     obj  = _.object(node.pairs);
-                // var keys = node.
-
-                return <ol className="tree-outline component-root platform-mac source-code object-properties-section">
-                    <li className={classNames({"parent": 1, expanded})} onClick={this.toggleExpand}>
-                        <div className="selection"></div>
-                        <content>
-                            <span className="console-object-preview console-line">
-                                Object {"{"}
-                                {
-                                    keys.map((key, i, a) => {
-                                        return [
-                                            <span className="name">{key}</span>,
-                                            ": ",
-                                            <ObjectPreview node={obj[key]} />,
-                                            i == a.length - 1 ? "" : ", "
-                                        ]
-                                    })
-                                }
-                                {"}"}
-                            </span>
-                        </content>
-                    </li>
-                    {!expanded ? <ol className="children" /> : <ol className="children expanded">
-                        {
-                            keys.map((key, i, a) => {
-                                return <ObjectSubtree key={key} name={key} parent={obj} />
-                            })
-                        }
-                    </ol>}
-                </ol>
-            }else if(node.type == 'function'){
-                return <span className="console-message-text source-code">
-                    <span className="object-value-function">{node.name || 'anonymous'}</span>()
+                return <span className="console-object-preview console-line">
+                    Object {"{"}
+                    {array_join(keys.map(key => [
+                        <span className="name">{key}</span>,
+                        ": ",
+                        <ObjectPreview node={obj[key]} />
+                    ]), ', ')}
+                    {"}"}
                 </span>
-            }else if(node.type == 'undefined'){
-                return <span className="object-value-undefined source-code">undefined</span>
-            }else if(node.type == 'null'){
-                return <span className="object-value-null source-code">null</span>
-            }else if(node.type == "regexp"){
-                return <span className="object-value-regexp source-code">{node.code}</span>
-            }else{
-                return <span>wat? {JSON.stringify(node)}</span>
             }
         }
-        return <div>hi</div>
+        return <ObjectPreview node={node} / >
     }
 }
 
-class ObjectSubtree extends Component {
+export class ExpandableTree extends Component {
     constructor() {
         super()
-        this.state = {
-            expanded: false
-        }
+        this.state = { expanded: false }
     }
-    toggleExpand = () => {
-        this.setState({ expanded: !this.state.expanded })
-    }
-    render() {
+    toggleExpand = () => { this.setState({ expanded: !this.state.expanded }) }
+    render(){
+        var {node} = this.props;
         var {expanded} = this.state;
+        var keys = _.pluck(node.pairs, 0),
+            obj  = _.object(node.pairs);
+        return (
+            <ol className="tree-outline component-root platform-mac source-code object-properties-section">
+                <li className={classNames({"parent": 1, expanded})} onClick={this.toggleExpand}>
+                    <div className="selection"></div>
+                    <content>
+                        <ObjectFatPreview node={node} />
+                    </content>
+                </li>
+                {!expanded ? <ol className="children" /> : <ol className="children expanded">{
+                    keys.map(key => <LabelPair key={key} name={key} parent={obj} />)
+                }</ol>}
+            </ol>
+        )
+    }
+}
+
+export class LabelPair extends Component {
+    constructor() {
+        super()
+        this.state = { expanded: false }
+    }
+    toggleExpand = () => { this.setState({ expanded: !this.state.expanded }) }
+    render(){
         var {parent, name} = this.props;
-        var desc = Object.getOwnPropertyDescriptor(parent, name)
-        // console.log(name, desc, desc.get)
-        if(desc.get){
-            return <li>
-                <div className="selection"></div>
-                <span className={classNames({name: 1, "object-properties-section-dimmed": !desc.enumerable})}>{name}</span>
-                <span className="object-properties-section-separator">: </span>
-                ...
-            </li>;
-        }
         var node = parent[name];
-        if(typeof node != "object" || node === null) {
-            return <li>
-                <div className="selection"></div>
-                <span className={classNames({name: 1, "object-properties-section-dimmed": !desc.enumerable})}>{name}</span>
-                <span className="object-properties-section-separator">: </span>
-                <ObjectPreview node={node} />
-            </li>;
+        var {expanded} = this.state;
+        var desc = { enumerable: true };
+        if(node.type == 'array' || node.type == 'object'){
+            return (
+                <div>
+                    <li className={classNames({"parent": 1, expanded})} onClick={this.toggleExpand}>
+                        <div className="selection"></div>
+                        <span className={classNames({name: 1, "object-properties-section-dimmed": !desc.enumerable})}>{name}</span>
+                        <span className="object-properties-section-separator">: </span>
+                        <ObjectPreview node={node} />
+                    </li>
+                    {!expanded ? <ol className="children" /> : <ol className="children expanded">{
+                        JSON.stringify(node)
+                    }</ol>}
+                </div>
+            );
+        }else{
+            return (
+                <li>
+                    <div className="selection"></div>
+                    <span className={classNames({name: 1, "object-properties-section-dimmed": !desc.enumerable})}>{name}</span>
+                    <span className="object-properties-section-separator">: </span>
+                    <ObjectPreview node={node} />
+                </li>
+            )
         }
-        return <div>
-            <li className={classNames({"parent": 1, expanded})}  onClick={this.toggleExpand}>
-            <div className="selection"></div>
-            <span className={classNames({name: 1, "object-properties-section-dimmed": !desc.enumerable})}>{name}</span>
-            <span className="object-properties-section-separator">: </span>
-            <ObjectPreview node={node} />
-            
-        </li>
-        {!expanded ? <ol className="children" /> : <ol className="children expanded">
-                {
-                    Object.getOwnPropertyNames(node).map((key, i, a) => {
-                        return <ObjectSubtree key={key} name={key} parent={node} />
-                    })
-                }
-            </ol>}
-        </div>
-    }    
+        
+    }
+}
+
+
+export default class ObjectTree extends Component {
+    render() {
+        var {node} = this.props;
+        if(node.type == 'array' || node.type == 'object'){
+            return <ExpandableTree node={node} />
+        }else{
+            return <ObjectPreview node={node} />
+        }
+    }
 }
